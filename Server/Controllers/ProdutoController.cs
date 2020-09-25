160d6a8d09e81bd616bbd46e41c53fe1e1ed4ca9
MatthewTakeshi <mthamamoto@hotmail.com>
2020-09-25 17:59:08 -0300

    CRUD

--- Server/Controllers/ProdutoController.cs
+++ Server/Controllers/ProdutoController.cs
@@ -4,6 +4,7 @@ using Desafio_CRUD.Shared;
 using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 [ApiController]
 [Route("[controller]")]
@@ -22,6 +23,14 @@ public class ProdutoController : Controller
     {
         var Produtos = await db.Produtos.ToListAsync();
         return Ok(Produtos);
+    }
+
+    [HttpGet]
+    [Route("GetById")]
+    public async Task<IActionResult> Get([FromQuery] string id)
+    {
+        var user = await db.Produtos.SingleOrDefaultAsync(x => x.ID == Convert.ToInt32(id));
+        return Ok(user);
     }
 
     [HttpPost]
@@ -47,6 +56,47 @@ public class ProdutoController : Controller
             return View(e);
         }
     }
+
+    [HttpPut]
+    [Route("Update")]
+    public async Task<IActionResult> Put([FromBody] Modelo_Produto Produto_Modificado)
+    {
+        db.Entry(Produto_Modificado).State = EntityState.Modified;
+        try
+        {
+            await db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw (ex);
+        }
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("Delete/{id}")]
+    public async Task<ActionResult<Modelo_Produto>> Delete(int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var user = await db.Produtos.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        db.Produtos.Remove(user);
+        await db.SaveChangesAsync();
+        return user;
+    }
+
+    private bool Modelo_ProdutoExists(int id)
+    {
+        return db.Produtos.Any(e => e.ID == id);
+    }
+
+
     /*
     [HttpGet]
     [Route("Teste")]
--- Shared/Modelo_Produto.cs
+++ Shared/Modelo_Produto.cs
@@ -6,11 +6,16 @@ using System.Text;
 
 namespace Desafio_CRUD.Shared
 {
+    
     public class Modelo_Produto
     {
+        [Required (ErrorMessage = "ID é obrigatório")]
         public int ID { get; set; }
+        [Required (ErrorMessage = "Nome é obrigatório")]
         public string Nome { get; set; }
+        [Required (ErrorMessage = "Descrição é obrigatório")]
         public string Descricao { get; set; }
+        [Required (ErrorMessage = "Valor é obrigatório")]
         public double Valor { get; set; }
     }
 }
